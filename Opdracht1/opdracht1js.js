47ef5b8364c916bcad302d257349bb567ec2c647
RalkeyOfficial <Ralkey@outlook.com>
2021-01-11 10:48:22 +0100

    added .js file

---
+++ Opdracht1/opdracht1js.js
@@ -0,0 +1,19 @@
+console.log("opdr 1.1")
+console.log("Hello World!\n\n")
+
+
+console.log("opdr 1.2")
+for (let index = 0; index < 10; index++) {
+    console.log(index)
+}
+console.log("\n\n")
+
+
+console.log('opdr 1.3')
+var mijnauto = {
+    merk: "Ford",
+    type: "Mondeo",
+    aantalwielen: 4,
+    kleur: "blauw"
+}
+console.log(mijnauto.kleur + "\n\n")
