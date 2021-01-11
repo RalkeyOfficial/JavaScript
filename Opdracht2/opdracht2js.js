d41c2ddf3ad4dc16dc2f7c9304f56abbc612a39f
RalkeyOfficial <Ralkey@outlook.com>
2021-01-11 11:34:25 +0100

    done with H2

--- Opdracht2/opdracht2js.js
+++ Opdracht2/opdracht2js.js
@@ -1,4 +1,13 @@
+//change element text
 document.getElementById("header").innerHTML = "Dit is de paginatitel"
 document.getElementById("paragraph").innerHTML = "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea  ommodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat  non proident, sunt in culpa qui officia  deserunt mollit anim id est laborum."
+
+//change image source
 document.getElementById("img1").src = "images/lohp-image-img-3.jpg"
 document.getElementById("img2").src = "images/mmtLbKa7jt4EoW8m9tzCp6-320-80.jpg"
+
+//change style attribute
+bl1.style.backgroundColor = "red"
+bl2.style.backgroundColor = "yellow"
+bl3.style.backgroundColor = "blue"
+bl4.style.backgroundColor = "green"
