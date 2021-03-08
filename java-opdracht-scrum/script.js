4dedd4a12bbbc3efce4b11724dc017442535eabb
RalkeyOfficial <Ralkey@outlook.com>
2021-03-08 09:02:31 +0100

    Update script.js

--- java-opdracht-scrum/script.js
+++ java-opdracht-scrum/script.js
@@ -1,25 +1,7 @@
 'use strict';
 
-/* STAPPENPLAN OPLOSSEN PROBLEEM */
-/* New Game starten =>
-* alle currentScores op 0, alle bankSCores op 0 en speler 1 mag beginnen,
-* dobbelsteen nog niet zien, kleur van de achtergrond is ook juist
-*
-* Roll Dice => plaatje aanpassen
-*   ALS score 1 => currentScore huidige speler moet naar 0, wisselSpeler (ook de achtergrond)
-*   ALS score != 1 => tel op bij CurrentScore
-*
-*
-* HOLD => tel currentScore op bij bankScore huidige speler, currentScore komt op 0,
-* ALS bank > winGetal,
-* DAN endGame =>winnaarskleurtje / wie is de winnaar (player 1 heeft gewonnen!)
-* / alles resetten (na 20 sec) of new game
-* ELSE wisselSpeler (ook de achtergrond)
 
-*/
-
-
-// ELEMENTEN
+// ELEMENTS
 const diceImageElement = document.querySelector(".dice");
 const rollDiceElement = document.querySelector(".btn--roll");
 const holdDiceElement = document.querySelector(".btn--hold");
@@ -34,19 +16,15 @@ const player2CurrentScoresElement = document.getElementById("current--1");
 const player1FinalScoresElement = document.getElementById("score--0");
 const player2FinalScoresElement = document.getElementById("score--1");
 
-// HULP VARIABELEN
-const currentScores = [0, 0];
-const finalScores = [0, 0];
+// EXTRA VARIABLES
+let currentScores = [0, 0];
+let finalScores = [0, 0];
 let currentPlayer = 0;
 let scoreToWin = 100;
 
 
-//!starting functions
-changeBG();
-
-
-//!EventListeners
-// ROLL DICE & ADD SCORES & SWITCH PLAYERS
+//!     <-- EventListeners -->
+//* ROLL DICE & ADD SCORES & SWITCH PLAYERS
 rollDiceElement.addEventListener("click", function(){
 
     const generatedNumber = GenerateRandomNumber(6);
@@ -64,7 +42,7 @@ rollDiceElement.addEventListener("click", function(){
     }
 })
 
-// HOLD CURRENT SCORES & CHECK IF WINNER & SWITCH PLAYERS
+//* HOLD CURRENT SCORES & CHECK IF WINNER & SWITCH PLAYERS
 holdDiceElement.addEventListener("click", function(){
     finalScores[currentPlayer] += currentScores[currentPlayer];
 
@@ -74,25 +52,25 @@ holdDiceElement.addEventListener("click", function(){
     switchPlayers();
 })
 
-// RESET GAME
+//* RESET GAME
 resetGameElement.addEventListener("click", function(){
     resetGame();
 })
 
 
-//!Functions
-//laat scores op scherm zien
+//!     <-- Functions -->
+//* laat scores op scherm zien
 function ShowScoresOnScreen() {
     [player1CurrentScoresElement.textContent, player2CurrentScoresElement.textContent] = currentScores;
     [player1FinalScoresElement.textContent, player2FinalScoresElement.textContent] = finalScores;
 }
 
-//reset de current scores van de current player
+//* reset current score of current player
 function resetCurrentScores() {
     currentScores[currentPlayer] = 0;
 }
 
-//switch van current player & change BG
+//* switch current player & change BG
 function switchPlayers() {
     //if current player = 0
     //  if true: current player = 1
@@ -101,7 +79,7 @@ function switchPlayers() {
     changeBG();
 }
 
-//check if current player won
+//* check if current player won0
 function checkIfWinner() {
     if (finalScores[currentPlayer] >= scoreToWin) {
         showVictoryScreen();
@@ -110,36 +88,29 @@ function checkIfWinner() {
     return false;
 }
 
-//show a alert that the current player won & reset game over 5 seconds
+//* show a alert that the current player won & reset game over 5 seconds
 function showVictoryScreen() {
     alert(`Player ${currentPlayer + 1} WON!`);
     setTimeout(resetGame, 3000);
 }
 
-//reset the game
+//* reset the game
 function resetGame() {
-    currentScores[0] = 0;
-    currentScores[1] = 0;
-    finalScores[0] = 0;
-    finalScores[1] = 0;
+    currentScores = [0,0];
+    finalScores   = [0,0];
 
     ShowScoresOnScreen();
 }
 
-//generate a random number
+//* generate a random number
 function GenerateRandomNumber(n) {
     return Math.floor(Math.random() * n) + 1;
 }
 
-//change background color
+//* change background color
 function changeBG() {
     // changes BG color by adding a class to the active player's background
-    if (currentPlayer === 0) {
-        backgroundPlayer1.classList.add("player--active");
-        backgroundPlayer2.classList.remove("player--active");
-    } else {
-        backgroundPlayer1.classList.remove("player--active");
-        backgroundPlayer2.classList.add("player--active");
-    }
+    backgroundPlayer1.classList.toggle("player--active");
+    backgroundPlayer2.classList.toggle("player--active");
 }
 
